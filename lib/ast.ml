a3b7a4de5cf554a4303f90e90d64d65b35a34d6b
Vladislav <morozvv75@gmail.com>
2026-03-06 16:35:49 +0300

    feat: add project config

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,22 +1,40 @@
+(* Синтаксическое дерево выражений MiniML *)
 type expr =
+  (* Целое число *)
   | Int of int
+  (* Булево значение *)
   | Bool of bool
+  (* Переменная *)
   | Var of string
+  (* Функция: параметр и тело *)
   | Fun of string * expr
+  (* Применение функции к аргументу *)
   | App of expr * expr
+  (* Локальное связывание *)
   | Let of string * expr * expr
+  (* Рекурсивное связывание функции *)
   | LetRec of string * string * expr * expr
+  (* Условное выражение *)
   | If of expr * expr * expr
+  (* Бинарная операция *)
   | BinOp of binop * expr * expr
+  (* Унарная операция *)
   | UnOp of unop * expr
 
+(* Бинарные операции *)
 and binop = Add | Sub | Mul | Div | Eq | Neq | Lt | Le | Gt | Ge | And | Or
+
+(* Унарные операции *)
 and unop = Neg | Not
 
+(* Значения времени выполнения *)
 type value =
   | IntValue of int
   | BoolValue of bool
+  (* Замыкание обычной функции *)
   | FunValue of string * expr * env
+  (* Замыкание рекурсивной функции *)
   | RecFunValue of string * string * expr * env
 
+(* Окружение: имя -> значение *)
 and env = (string * value) list
